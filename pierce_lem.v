6b65a3a923ac0f2e787ca9eaaf463947d3465b2d
Joona Piirainen <joona.piirainen@gmail.com>
2022-09-04 14:57:38 +0300

    My first coq theorem!

---
+++ pierce_lem.v
@@ -0,0 +1,15 @@
+(* My first Coq theorem. *)
+
+Definition pierce := forall (p q : Prop), ((p -> q) -> p) -> p.
+
+Definition lem := forall (p : Prop), p \/ ~ p.
+
+Theorem pierce_equiv_lem : pierce <-> lem.
+  unfold pierce, lem.
+  firstorder.
+  apply H with (q := ~(p \/ ~p)).
+  tauto.
+  destruct (H p).
+  assumption.
+  tauto.
+Qed.
